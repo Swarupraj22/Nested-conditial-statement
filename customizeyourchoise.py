1a47c44db19afc9931e80588cfa2bdddf4a7901e
Swarupraj22 <pswarupraj22@gmail.com>
2025-12-06 20:05:42 +0545

    initial commit

--- customizeyourchoise.py
+++ customizeyourchoise.py
@@ -1,25 +1,26 @@
-print("Select your ride: ")
+print("Select your choice: ")
 print("1. Bike")
 print("2. Car")
-choice=int(input("Enter the choice:"))
-if(choice==1):
-    print("What type of bike")
-    print("1. Scooty\n")
-    print("2. Scooter\n")
-    choice2=int(input("Enter the choice:"))
-    if choice2==1:
-        print("Scooty selected")
-    else:
-        print("Scooter selected")
+choice = int(input("Enter the choice: "))
 
-elif(choice==2):
-    print("What type of car")
-    print("1. Lamborghani\n")
-    print("2.Toyota")
-    choice2=int(input("Enter the choice:"))
-    if choice3==1:
-        print("Lamborghani")
+if choice == 1:
+    print("\nWhat type of bike")
+    print("1. Scooty")
+    print("2. Scooter")
+    choice2 = int(input("\nEnter the choice: "))
+    if choice2 == 1:
+        print("\nThe choice is: Scooty selected\n You can buy now")
     else:
-        print("Toyota")
+        print("\nThe choice is: Scooter\n You can buy now")
+
+elif choice == 2:
+    print("\nWhat type of car")
+    print("1. Lamborghani")
+    print("2. Toyota\n")
+    choice2 = int(input("Enter the choice: "))
+    if choice2 == 1:
+        print("\nThe choice is: Lamborghani\n You can buy now")
+    else:
+        print("\nThe choice is: Toyota\n You can buy now")
 else:
     print("Wrong Entry")
